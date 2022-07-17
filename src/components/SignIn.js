e795f62020dbeed96ec166168b9ab6133bdfde62
Tamer Mansour <78074218+Tamer-Mansour@users.noreply.github.com>
2022-07-17 16:05:45 +0300

    second

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,16 +4,32 @@ import { auth } from '../firebase.js'
 import { Button } from '@material-ui/core'
 
 function SignIn() {
-    function signInWithGoogle() {
-        const provider = new firebase.auth.GoogleAuthProvider()
-        auth.signInWithPopup(provider)
-    }
-    return (
-        <div style={{ display: 'flex', justifyContent: 'center', height: '100vh', alignItems: 'center' }}>
-            <Button style={{ padding: '30px', fontSize: '20px', borderRadius: '0', fontWeight: '600' }} onClick={signInWithGoogle}>Sign In With Google</Button>
-        </div>
-    )
+  function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+  }
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: '100vh',
+        alignItems: 'center',
+      }}
+    >
+      <Button
+        style={{
+          padding: '30px',
+          fontSize: '20px',
+          borderRadius: '0',
+          fontWeight: '600',
+        }}
+        onClick={signInWithGoogle}
+      >
+        Sign In With Google
+      </Button>
+    </div>
+  )
 }
 
 export default SignIn
-
